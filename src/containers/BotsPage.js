0085931b4eb5ca1b6e13798da592f8a981cedea9
Tr1ckC0 <trick.co@yahoo.com>
2020-05-20 10:25:28 -0500

    fetch

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 class BotsPage extends Component {
-  //start here with your code for step one
+  constructor() {
+    super();
+
+    this.state = {
+      bots: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setBots();
+  }
 
   render() {
+    console.log(this.state)
     return <div>{/* put your components here */}</div>;
   }
+
+  setBots = () => {
+    fetch("http://localhost:6001/bots")
+      .then((r) => r.json())
+      .then((j) => this.setState({
+        bots: j
+      }));
+  };
 }
 
 export default BotsPage;

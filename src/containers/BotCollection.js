0f0fb0360cb762f6784471211b712eaef5a37b33
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-05-20 10:51:01 -0500

    moved allBots state to BotCollecttion

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import BotCard from "../components/BotCard";
 
+const API = "http://localhost:6001/bots"
+
 class BotCollection extends Component {
-  //your code here
+  constructor() {
+    super();
+    this.state = {
+      allBots: []
+    }
+  }
+  
+  componentDidMount() {
+    fetch(API)
+      .then(r => r.json())
+      .then(json => this.setState({ allBots: json }))
+  }
+
 
   render() {
     return (
       <div className="ui four column grid">
         <div className="row">
-          {this.props.botList.map(bot => <BotCard bot={bot}/>)}
+          {this.state.allBots.map(bot => <BotCard bot={bot}/>)}
         </div>
       </div>
     );
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,24 +2,9 @@ import React, { Component } from "react";
 import BotCollection from "./BotCollection";
 import YourBotArmy from "./YourBotArmy";
 
-const API = "http://localhost:6001/bots"
-
 class BotsPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      allBots: []
-    }
-  }
-
-  componentDidMount() {
-    fetch(API)
-      .then(r => r.json())
-      .then(json => this.setState({allBots: json}))
-  }
-
   render() {
-    return <div><YourBotArmy /><BotCollection botList={this.state.allBots} /></div>;
+    return <div><YourBotArmy /><BotCollection /></div>;
   }
 }
 

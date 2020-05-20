f994fe3fdae760b68d7bcefe3e635b078cb412bc
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-05-20 12:03:14 -0500

    submission for deadline

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy'
+
+const API = "http://localhost:6001/bots"
 
 class BotsPage extends Component {
   //start here with your code for step one
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then((r) => r.json())
+      .then((data) => this.setState({ data }));
+  }
+
+  enlistRobot(e) {
+    e.persist()
+    console.log(e.target.src)
+    console.log(e.target)
+    console.log(e)
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return (
+      <div>
+        <YourBotArmy />
+        <BotCollection bots={this.state.data} handleClick={this.enlistRobot}/>
+      </div>
+    );
   }
 }
 

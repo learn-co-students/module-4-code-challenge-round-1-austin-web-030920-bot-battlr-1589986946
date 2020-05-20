442ffa72c41acc81a668b67c6b47b2916baa4b82
github username <github email address>
2020-05-20 11:59:50 -0500

    submission for deadline

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard'
 
 class YourBotArmy extends Component {
-  //your bot army code here...
+  generateBot = () => {
+    let bots = this.props.userBots
+    return bots.map(bot => <BotCard 
+       id = {bot.id}
+       name = {bot.name}
+       health = {bot.health}
+       damage = {bot.damage}
+       armor = {bot.armor}
+       bot_class = {bot.bot_class}
+       catchphrase = {bot.catchphrase}
+       avatar_url = {bot.avatar_url}
+       handleBotClick = {this.props.handleRemoveBotClick}
+       handleDeleteClick = {this.props.handleDeleteBotClick}
+      />)
+    }
 
   render() {
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.generateBot()}
           </div>
         </div>
       </div>
